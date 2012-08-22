e9ee2052792e9f483515131f37aa9b2c7302bd35
Morten Rønne <mron@dtic.dtu.dk>
2012-08-22 13:39:04 +0200

    Updated supply model to has_many_belongs_to_many providers.

--- app/models/supply.rb
+++ app/models/supply.rb
@@ -1,6 +1,8 @@
 class Supply < ActiveRecord::Base
   attr_accessible :code, :description, :id
 
+  has_and_belongs_to_many :providers
+
   validates :code,  :presence => true
   validates :description, :presence => true,
                     :length => { :minimum => 5 }
--- db/migrate/20120817072849_create_supplies.rb
+++ db/migrate/20120817072849_create_supplies.rb
@@ -2,8 +2,8 @@ class CreateSupplies < ActiveRecord::Migration
   def change
     create_table :supplies do |t|
       t.primary_key :id
-      t.string :code, :limit => 10
-      t.text :description
+      t.string :code, :limit => 10, :null => false
+      t.text :description, :null => false
 
       t.timestamps
     end
