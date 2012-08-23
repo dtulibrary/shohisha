8487490f3473529cf9cdb02a67af012816223b4d
Morten Rønne <mron@dtic.dtu.dk>
2012-08-23 09:53:33 +0200

    Added Ip Address to provider form.

--- app/models/ipaddress.rb
+++ app/models/ipaddress.rb
@@ -1,4 +1,6 @@
 class Ipaddress < ActiveRecord::Base
   belongs_to :provider
   attr_accessible :ipaddress
+  
+  validates :ipaddress, :presence => true
 end
--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,12 +1,15 @@
 class Provider < ActiveRecord::Base
-  attr_accessible :description, :code, :supply_ids, :consumers_providers_attributes, :packages_attributes
+  attr_accessible :description, :code, :supply_ids, :consumers_providers_attributes, :packages_attributes,
+    :ipaddresses_attributes
 
   has_and_belongs_to_many :supplies
   has_many :packages
   has_many :consumers_providers
   has_many :consumers, :through => :consumers_providers
+  has_many :ipaddresses
   accepts_nested_attributes_for :consumers_providers, :allow_destroy => true
   accepts_nested_attributes_for :packages, :allow_destroy => true
+  accepts_nested_attributes_for :ipaddresses, :allow_destroy => true
 
   validates :code,  :presence => true
   validates :description, :presence => true
