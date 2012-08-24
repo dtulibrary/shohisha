9ff4b56b7b544e37dc45fe794fcd3fe26e3c76cc
Morten Rønne <mron@dtic.dtu.dk>
2012-08-24 12:10:07 +0200

    Manual updates to status.

--- db/migrate/20120824095435_create_statuses.rb
+++ db/migrate/20120824095435_create_statuses.rb
@@ -1,8 +1,8 @@
 class CreateStatuses < ActiveRecord::Migration
   def change
     create_table :statuses do |t|
-      t.string :code
-      t.string :description
+      t.string :code, :null => false
+      t.string :description, :null => false
 
       t.timestamps
     end
