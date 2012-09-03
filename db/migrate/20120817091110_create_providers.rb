e7611fed0ae4cedcdd74aa575c9b4e5d17e2067d
Morten Rønne <morten.roenne@tdcadsl.dk>
2012-09-03 09:54:45 +0200

    Fixed missing colons in front of null definitions.

--- db/migrate/20120817091110_create_providers.rb
+++ db/migrate/20120817091110_create_providers.rb
@@ -2,7 +2,7 @@ class CreateProviders < ActiveRecord::Migration
   def change
     create_table :providers do |t|
       t.string :code, :limit => 30, :null => false
-      t.string :description, null => false
+      t.string :description, :null => false
 
       t.timestamps
     end
--- db/migrate/20120820062359_create_providers_supplies.rb
+++ db/migrate/20120820062359_create_providers_supplies.rb
@@ -2,7 +2,7 @@ class CreateProvidersSupplies < ActiveRecord::Migration
   def up
     create_table :providers_supplies, :id => false do |t|
       t.references provider, :null => false
-      t.references supply, null => false
+      t.references supply, :null => false
     end
   end
 
