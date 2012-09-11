596d65f1f86dc95734f9b8578c176886ce6167c8
Morten Rønne <morten.roenne@tdcadsl.dk>
2012-09-11 09:25:25 +0200

    Updated fetchers with missing accessible attributes and correct display of linked values.

--- app/models/fetcher.rb
+++ app/models/fetcher.rb
@@ -3,5 +3,8 @@ class Fetcher < ActiveRecord::Base
   belongs_to :status
   belongs_to :transport
   belongs_to :retain
-  attr_accessible :basename, :charset, :directory, :directory_filter, :file_filter, :format, :passive, :password, :recursive, :remote_delete, :seperator, :set, :set_limit, :timeout, :username
+  attr_accessible :basename, :charset, :directory, :directory_filter,
+    :file_filter, :format, :passive, :password, :recursive, :remote_delete,
+    :seperator, :set, :set_limit, :timeout, :username, :provider_id,
+    :status_id, :transport_id, :retain_id
 end
