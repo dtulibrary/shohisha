9c638d365f5691165af713f017fd1cc76df29275
Morten Rønne <morten.roenne@tdcadsl.dk>
2013-03-08 14:38:08 +0100

    Added user validation rules and tests.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   devise :cas_authenticatable
 
   attr_accessible :username
+
+  validates :username, :presence => true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    FactoryGirl.create(:user).should be_valid
+  end
+
+  it "fails without username" do
+    @provider = FactoryGirl.build(:user, username: nil).should_not be_valid
+  end
+
 end
