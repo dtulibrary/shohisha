aab2687862554741af78ef95de1f87fcb6022312
Morten Rønne <morten.roenne@tdcadsl.dk>
2013-03-12 15:58:57 +0100

    Updated schema.rb.

--- db/schema.rb
+++ db/schema.rb
@@ -130,14 +130,14 @@ ActiveRecord::Schema.define(:version => 20130214135717) do
   end
 
   create_table "providers", :force => true do |t|
-    t.string   "code"
-    t.string   "description"
-    t.datetime "created_at",                      :null => false
-    t.datetime "updated_at",                      :null => false
-    t.integer  "provider_type_id", :default => 1, :null => false
+    t.string   "code",             :limit => 30,                :null => false
+    t.string   "description",                                   :null => false
+    t.datetime "created_at",                                    :null => false
+    t.datetime "updated_at",                                    :null => false
+    t.integer  "provider_type_id",               :default => 1, :null => false
   end
 
-  add_index "providers", ["code"], :name => "index_providers_on_short_name", :unique => true
+  add_index "providers", ["code"], :name => "index_providers_on_code", :unique => true
 
   create_table "providers_supplies", :id => false, :force => true do |t|
     t.integer "provider_id", :null => false
@@ -169,8 +169,8 @@ ActiveRecord::Schema.define(:version => 20130214135717) do
   end
 
   create_table "supplies", :force => true do |t|
-    t.string   "code",        :limit => 10
-    t.text     "description"
+    t.string   "code",        :limit => 10, :null => false
+    t.text     "description",               :null => false
     t.datetime "created_at",                :null => false
     t.datetime "updated_at",                :null => false
   end
