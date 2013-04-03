4fd225825c4f53c54e950b1c13dafda1e494d118
Morten Rønne <morten.roenne@tdcadsl.dk>
2013-04-03 11:38:17 +0200

    Added consumer delete restriction to package.

--- app/models/package.rb
+++ app/models/package.rb
@@ -2,7 +2,7 @@ class Package < ActiveRecord::Base
   belongs_to :provider
   attr_accessible :code, :description, :provider_id, :consumers_packages_attributes
   
-  has_many :consumers_packages
+  has_many :consumers_packages, :dependent => :restrict
   has_many :consumers, :through => :consumers_packages
 
   accepts_nested_attributes_for :consumers_packages, :allow_destroy => true
