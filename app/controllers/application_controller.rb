063691f7e4dc51407fd5d123841820bd6a63e145
Morten Rønne <morten.roenne@tdcadsl.dk>
2013-05-02 11:22:49 +0200

    Removed unused function (best_practices).

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  def authenticate_admin_user!
-    #if 
-    #current_user =
-    #unless current_user.admin?
-    #  flash[:alert] = I18n.t("shohisha.admin")
-    #  redirect_to root_path
-    #end
-  end
-
 end
