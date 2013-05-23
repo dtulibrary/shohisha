311f532df5f66de5d6a9a5884768b78b796ef8f1
Morten Rønne <morten.roenne@tdcadsl.dk>
2013-05-23 12:33:22 +0200

    Added test for Ipaddress admin interface.

---
+++ spec/controllers/admin/ipaddresses_controller_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+include Devise::TestHelpers
+
+describe Admin::IpaddressesController do
+  render_views
+
+  before(:each) do
+    @user = FactoryGirl.create(:user)
+    sign_in @user
+  end
+
+  after(:each) do
+    @user.destroy
+  end
+
+  describe "GET #index" do
+    it "renders the :index view" do
+      ipaddress = FactoryGirl.create(:ipaddress)
+      get :index
+      response.status.should be(200)
+      response.should render_template :index
+    end
+  end
+
+  describe "GET #show" do
+    it "renders the #show view" do
+      get :show, id: FactoryGirl.create(:ipaddress)
+      response.status.should be(200)
+      response.should render_template :show
+    end
+  end
+
+  describe "GET #new" do
+    it "shows the new template" do
+      get :new
+      response.status.should be(200)
+      response.should render_template :new
+    end
+  end
+
+  describe "GET #edit" do
+    it "shows the edit template" do
+      get :edit, id: FactoryGirl.create(:ipaddress)
+      response.status.should be(200)
+      response.should render_template :edit
+    end
+  end
+
+end
